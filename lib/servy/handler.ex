711357db458a13d36ea0a92025951d4f5576fba0
Muzhawir Amri <muzhawir.amri@gmail.com>
2024-12-07 19:59:29 +0800

    Completed Chapter 07 - Advanced Pattern Matching (#2)

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -18,26 +18,58 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status: nil}
   end
 
   def route(conv) do
     route(conv, conv.method, conv.path)
   end
 
-  def route(conv, "GET", "/wildthings"), do: %{conv | resp_body: "Bears, Lions, Tigers"}
-  def route(conv, "GET", "/bears"), do: %{conv | resp_body: "Teddy, Smoky, Paddington"}
+  def route(conv, "GET", "/wildthings") do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | status: 200, resp_body: "Teddy, Smoky, Paddington"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, "DELETE", "/bears/" <> _id) do
+    %{conv | status: 403, resp_body: "Deleting bear is forbidden"}
+  end
+
+  def route(conv, _method, path) do
+    %{conv | status: 404, resp_body: "No Path #{path} here"}
+  end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    http_response_codes = %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }
+
+    http_response_codes[code]
+  end
 end
+
+# Wildthings
 
 request = """
 GET /wildthings HTTP/1.1
@@ -51,6 +83,8 @@ response = Servy.Handler.handle(request)
 
 IO.puts(response)
 
+# Bears
+
 request_bears = """
 GET /bears HTTP/1.1
 Host: example.com
@@ -63,14 +97,44 @@ response_bears = Servy.Handler.handle(request_bears)
 
 IO.puts(response_bears)
 
-# request_bigfoot = """
-# GET /bigfoot HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
+# Bears 1
 
-# """
+request_bears_1 = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
 
-# response_bigfoot = Servy.Handler.handle(request_bigfoot)
+"""
 
-# IO.puts(response_bigfoot)
+response_bears_1 = Servy.Handler.handle(request_bears_1)
+
+IO.puts(response_bears_1)
+
+# Bigfoot
+
+request_bigfoot = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response_bigfoot = Servy.Handler.handle(request_bigfoot)
+
+IO.puts(response_bigfoot)
+
+# Excercise DELETE METHOD
+
+request_delete = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response_delete = Servy.Handler.handle(request_delete)
+
+IO.puts(response_delete)
