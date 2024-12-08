eec34763cfcf4668ad814b7147cb6823f795299e
muzhawir <muzhawir.amri@gmail.com>
2024-12-09 01:21:37 +0800

    Completed Module 10 - Module Attribute

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,8 +1,14 @@
 defmodule Servy.Handler do
-  @moduledoc false
-
+  @moduledoc """
+  Handles HTTP requests.
+  """
   require Logger
 
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc """
+  Transforms a request into a response.
+  """
   def handle(request) do
     request
     |> parse()
@@ -14,35 +20,9 @@ defmodule Servy.Handler do
     |> format_response()
   end
 
-  def emojify(%{status: 200} = conv) do
-    emojis = String.duplicate("🎉", 2)
-
-    body = "#{emojis}\n#{conv.resp_body}\n#{emojis}"
-
-    %{conv | resp_body: body}
-  end
-
-  def emojify(conv), do: conv
-
-  def track(%{status: 404, path: path} = conv) do
-    Logger.warning("Warning #{path} is on the loose!")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "/bears/#{id}"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect(conv)
-
+  @doc """
+  Parses a request into a map.
+  """
   def parse(request) do
     [method, path, _] =
       request
@@ -53,10 +33,27 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: "", status: nil}
   end
 
-  # def route(conv) do
-  #   route(conv, conv.method, conv.path)
-  # end
+  @doc """
+  Rewrites the path based on the request path.
+  """
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
 
+  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+    %{conv | path: "/bears/#{id}"}
+  end
+
+  def rewrite_path(conv), do: conv
+
+  @doc """
+  Logs and inspects the request.
+  """
+  def log(conv), do: IO.inspect(conv)
+
+  @doc """
+  Routes a request.
+  """
   def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
@@ -73,18 +70,16 @@ defmodule Servy.Handler do
     %{conv | status: 403, resp_body: "Deleting bear is forbidden"}
   end
 
-  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
-    "../../pages"
-    |> Path.expand(__DIR__)
-    |> Path.join(file <> ".html")
+  def route(%{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
-    "../../pages"
-    |> Path.expand(__DIR__)
-    |> Path.join("about.html")
+  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+    @pages_path
+    |> Path.join(file <> ".html")
     |> File.read()
     |> handle_file(conv)
   end
@@ -93,6 +88,32 @@ defmodule Servy.Handler do
     %{conv | status: 404, resp_body: "No Path #{path} here!"}
   end
 
+  @doc """
+  Injects emoji into the response body.
+  """
+  def emojify(%{status: 200} = conv) do
+    emojis = String.duplicate("🎉", 2)
+
+    body = "#{emojis}\n#{conv.resp_body}\n#{emojis}"
+
+    %{conv | resp_body: body}
+  end
+
+  def emojify(conv), do: conv
+
+  @doc """
+  Tracks the request based on the status.
+  """
+  def track(%{status: 404, path: path} = conv) do
+    Logger.warning("Warning #{path} is on the loose!")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  @doc """
+  Formats the response.
+  """
   def format_response(conv) do
     """
     HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
@@ -244,7 +265,7 @@ IO.puts(response_about)
 # Bears New
 
 request_bears_new = """
-POST /bears/new HTTP/1.1
+GET /pages/form HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
