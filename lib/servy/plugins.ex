b13ea86fe352396e27f730e8041436bf5aaccb65
muzhawir <muzhawir.amri@gmail.com>
2024-12-10 20:39:58 +0800

    Completed Module 12 - Modeling With Structs

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -2,31 +2,32 @@ defmodule Servy.Plugins do
   @moduledoc """
   Handles HTTP Plugins.
   """
+  alias Servy.Conv
 
   require Logger
 
   @doc """
   Rewrites the path based on the request path.
   """
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+  def rewrite_path(%Conv{path: "/bears?id=" <> id} = conv) do
     %{conv | path: "/bears/#{id}"}
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path(%Conv{} = conv), do: conv
 
   @doc """
   Logs and inspects the request.
   """
-  def log(conv), do: IO.inspect(conv)
+  def log(%Conv{} = conv), do: IO.inspect(conv)
 
   @doc """
   Injects emoji into the response body.
   """
-  def emojify(%{status: 200} = conv) do
+  def emojify(%Conv{status: 200} = conv) do
     emojis = String.duplicate("🎉", 2)
 
     body = "#{emojis}\n#{conv.resp_body}\n#{emojis}"
@@ -34,15 +35,15 @@ defmodule Servy.Plugins do
     %{conv | resp_body: body}
   end
 
-  def emojify(conv), do: conv
+  def emojify(%Conv{} = conv), do: conv
 
   @doc """
   Tracks the request based on the status.
   """
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     Logger.warning("Warning #{path} is on the loose!")
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 end
