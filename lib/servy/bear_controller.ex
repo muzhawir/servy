60c30b92e2d4d748e4db169314f33843fa16ab5a
muzhawir <muzhawir.amri@gmail.com>
2024-12-14 23:51:26 +0800

    Completed Module 16 - Comprehensions

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,28 +3,22 @@ defmodule Servy.BearController do
   Handles HTTP requests to /bears
   """
 
+  import Servy.View, only: [render: 3]
+
   alias Servy.Bear
   alias Servy.Conv
   alias Servy.Wildthings
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
-  end
-
   def index(conv) do
-    items =
-      Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
-      |> Enum.sort(&Bear.order_ascending_by_name/2)
-      |> Enum.map_join(&bear_item/1)
+    bears = Enum.sort(Wildthings.list_bears(), &Bear.order_ascending_by_name/2)
 
-    %Conv{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    %Conv{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type} = _params) do
