31a19b95aa7517f31b0861a0fec58540af085d07
Muzhawir Amri <muzhawir.amri@gmail.com>
2024-12-16 06:05:15 +0800

    Completed Module 19 - Test Automation (#11)
    - Completed Module 19 - Test Automation
    - Update README

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -9,9 +9,9 @@ defmodule Servy.Parser do
   Parses a request into a struct
   """
   def parse(request) do
-    [top, params_string] = String.split(request, "\n\n", parts: 2)
+    [top, params_string] = String.split(request, "\r\n\r\n", parts: 2)
 
-    [request_line | header_lines] = String.split(top, "\n")
+    [request_line | header_lines] = String.split(top, "\r\n")
 
     [method, path, _] = String.split(request_line, " ")
 
@@ -29,18 +29,39 @@ defmodule Servy.Parser do
     }
   end
 
-  defp parse_headers(header_lines) do
+  @doc """
+  Parses header lines into a map
+
+  ## Examples
+
+      iex> Servy.Parser.parse_headers(["Host: example.com", "User-Agent: ExampleBrowser/1.0"])
+      %{"Host" => "example.com", "User-Agent" => "ExampleBrowser/1.0"}
+
+  """
+  def parse_headers(header_lines) do
     Enum.reduce(header_lines, %{}, fn line, header_so_far ->
       [key, value] = String.split(line, ": ")
       Map.put(header_so_far, key, value)
     end)
   end
 
-  defp parse_params("application/x-www-form-urlencoded", params_string) do
+  @doc """
+  Parses a form URL encoded string into a map
+
+  ## Examples
+
+      iex> Servy.Parser.parse_params("application/x-www-form-urlencoded", "name=Dave&type=Fish")
+      %{"name" => "Dave", "type" => "Fish"}
+
+      iex> Servy.Parser.parse_params("multipart/form-data", "name=Dave&type=Fish")
+      %{}
+
+  """
+  def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string
     |> String.trim()
     |> URI.decode_query()
   end
 
-  defp parse_params(_, _), do: %{}
+  def parse_params(_, _), do: %{}
 end
--- test/servy_test.exs
+++ test/servy_test.exs
@@ -1,5 +1,9 @@
 defmodule ServyTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   doctest Servy
+
+  test "greets the world" do
+    assert 1 + 1 == 2
+  end
 end
